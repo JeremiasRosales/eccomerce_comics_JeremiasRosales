8a872255128130395b662e36be5067a8c86304cd
jitsu <jeremiasrosalesfiore@gmail.com>
2023-10-07 14:00:05 -0300

    Update de datos

---
+++ eccomerce_JeremiasRosales/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import cart from "./assets/cart.png"
+
+const CartWidget = () =>
+{
+    return (
+        <div>
+            <img src={cart} alt="Cart-widget" style={{width: "30px"}}/>
+            0  
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ eccomerce_JeremiasRosales/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () =>
+{
+    return (
+        <nav>
+            <h3>Tienda de Cómics</h3>
+            <div>
+                <button>Marvel</button>
+                <button>DC</button>
+                <button>Indie</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
